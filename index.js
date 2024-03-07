3501397ddb2a28f8cde2f6e756774d73f95868ae
sivani-0719 <121398925+sivani-0719@users.noreply.github.com>
2024-03-07 22:23:31 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html >
+<head>
+    <title> background color</title>
+    <link rel="stylesheet" href="StyleSheet2.css" />
+    
+</head>
+<body>
+    <h3 id="height">Background Color : <span id="blue">#<span id="colorCode">59CC83</span></span></h3><br />
+            <button onclick="bgChange()"  onmouseover="black()" onmouseout="normal()"><strong>CLICK ME</strong></button>
+            <script type="text/javascript" src="javas.js"></script>
+    </body>
+    </html>
